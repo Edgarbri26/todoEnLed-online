a8c5d37a11253624b342ec53a0e3be59e59f2673
Jose Fernandez <josef310798@gmail.com>
2025-06-05 11:20:04 -0400

    search mejorado

--- src/php/search.php
+++ src/php/search.php
@@ -1,3 +1,27 @@
+<?php
+  session_start();
+  $_SESSION['rol'] = 2;
+  //esto por ahora funciona con js
+  // Inicializa el carrito si no existe
+
+  if (!isset($_SESSION['carrito'])) {
+      $_SESSION['carrito'] = [];
+      $_SESSION['numProducts'] = 0;
+  }
+
+  function agregarAlCarrito($producto) {
+      $_SESSION['carrito'][] = $producto;
+      $_SESSION['numProducts']++;
+  }
+
+  // aqui se agrega un producto
+  if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['producto'])) {
+      agregarAlCarrito($_POST['producto']);
+  }
+
+  
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,16 +34,57 @@
     <title>TodoEnLed</title>
 </head>
 <body>
-    
 
+<header>
+    <div class="container-header">
+      <div class="logo">
+        <a href="../../index.php" >
+        <img src="../../assets/logo.png" alt="Logo TodoEnLed">
+        </a>
+      </div>
+
+      <div class="container-search">
+        <form action="" method="post" >
+          <input type="search" placeholder="Buscar..." name="text" class="search-input">
+          <button type="submit" class="search-button" name="search">
+              <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+          </form>
+      </div>
+      
+      <button id="btn-login">login</button>
+
+<?php
+  if($_SESSION['rol'] != 3 ){
+    echo"<a href='src/php/cart.php' class='container-cart'>
+          <svg class='' width='28' height='28' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+              <g fill='currentColor'>
+                  <path fill-rule='evenodd' clip-rule='evenodd' d='M1.8 2.2a.8.8 0 1 0 0 1.6h2.803l1.542 14.541a1 1 0 0 0 .995.895h15.055a1 1 0 0 0 .982-.811l2.105-10.91a1 1 0 0 0-.982-1.189H6.48l-.432-4.081a.05.05 0 0 0-.05-.045H1.8Zm4.849 5.726h16.923l-1.873 9.709H7.679l-1.03-9.709Z'></path>
+                  <path d='M10.917 23.62a1.909 1.909 0 1 1-3.818 0 1.909 1.909 0 0 1 3.818 0ZM22.372 23.62a1.91 1.91 0 1 1-3.819 0 1.91 1.91 0 0 1 3.819 0Z'></path>
+              </g>
+          </svg>
+          <h3 id='counter-product'>
+          </h3>
+      </a>";
+  }else{
+
+  }
+?>
+    </div>
+
+  </header>
+
+<h1>Resultados</h1>
 
 <?php 
 include 'db.php';
 if(isset($_POST['search'])){
     $search = $_POST['text'];
-    $query = "SELECT * FROM products WHERE nombre = '$search'";
+    $query = "SELECT * FROM products WHERE nombre LIKE '%$search%';";
     $result = $conn->query($query);
-        while($row = $result->fetch_assoc()) {
+        if($result){
+            if($result->num_rows>0){
+                while($row = $result->fetch_assoc()) {
 
             echo "<div class='product-container'>
                 <a id={$row['id']} href='src/php/product.php'>
@@ -39,7 +104,11 @@ if(isset($_POST['search'])){
                         <span class='text'>Add</span>
                 </button>
                 </div>";
-      }
+                }
+            }else {
+                echo "No se encontro ese producto";
+            }
+        }
     }
 
 ?>
