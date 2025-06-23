6e7ba1cee87b188ee1a66507553a3d4c366da0ce
Jose Fernandez <josef310798@gmail.com>
2025-06-22 22:58:31 -0400

    index.php corregido

--- Public/index.php
+++ Public/index.php
@@ -4,15 +4,15 @@ require_once 'db.php';
 
 try {
     $conexion = Conectar::Conexion();
+    header("Location: /todoEnLed-online/app/Controllers/controller_index.php");
+    exit();
 
-    require_once '../app/controllers/controller_index.php';
-    $controller = new Product();
-    // Aquí continúa la lógica normal con la conexión exitosa...
 } catch (Exception $e) {
     // Incluir el controlador de error
     header("Location: /todoEnLed-online/app/Controllers/controller_error.php");
     exit();
 }
+
 
 
 
