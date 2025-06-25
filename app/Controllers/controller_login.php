3103a0c14271e80c5205e7c2be8a53a6fb5dfd05
Jose Fernandez <josef310798@gmail.com>
2025-06-25 17:12:05 -0400

    Ahora se carga la cantidad de productos en el carrito al iniciar sesion

--- app/Controllers/controller_login.php
+++ app/Controllers/controller_login.php
@@ -1,9 +1,11 @@
 <?php
 
 require_once __DIR__ . '/../models/model_login.php';
+require_once __DIR__ . '/../models/model_cart.php';
 require_once __DIR__ . '/../../Public/db.php';
 
 $login = new Login();
+$cart = new Cart();
 
 if (isset($_POST['button'])) {
 
@@ -11,6 +13,8 @@ if (isset($_POST['button'])) {
     $password = $_POST['password'];
 
     if ($login->authenticate($username, $password)) {
+        $id_user = $_SESSION['id_user'];
+        $cart->getNumProducts($id_user);
         header("Location: ../Controllers/controller_index.php");
         exit();
     } else {
